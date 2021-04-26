ec59f81f7254b4b5455777a055c46858fe1442e8
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-04-26 12:39:27 +0800

    AddTodoForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
+import AddTodoForm from './AddTodoForm'
 import TodoList from "./TodoList"
 
 
 const initialTodos: Array<Todo> = [
-  {text: 'learn basic typescript', complete: true},
-  {text: 'learn basic typescript', complete: true},
-  {text: 'learn basic typescript', complete: true}
+  // {text: 'learn basic typescript', complete: true},
 ]
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState(initialTodos)
+
+  const addTodo: AddTodo = (newTodo) => {
+    setTodos([...todos, {text: newTodo, complete: false}])
+  }
   
   return (
     <div>
       <TodoList todos={todos}/>
+      <AddTodoForm addTodo={addTodo}/>
     </div>
   )
 }
